78184b363fb594d5be12808cd07b93aeace65fd5
Eli Block <18074026+eliblocks@users.noreply.github.com>
2025-12-26 11:41:41 -0800

    add messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, :registerable,
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  has_many :messages
 end
